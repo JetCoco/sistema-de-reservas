7890b864091e2d1c52dae11fd35e182235f16361
Eduardo Torrentera <eduardo.torrentera.colin@gmail.com>
2025-07-03 20:12:49 -0600

    fix token

--- frontend/logout.js
+++ frontend/logout.js
@@ -1,15 +1,19 @@
 // logout.js
+import { userManager } from "./main.js";
 
-(function () {
-  const domain = 'https://us-east-1peioypcf6.auth.us-east-1.amazoncognito.com';
-  const clientId = '1a43cn452jearoj6siqsnj70g5';
-  const logoutRedirect = 'https://sistema-reservas-frontend.s3.us-east-1.amazonaws.com/index.html';
+(async () => {
+  try {
+    await userManager.signoutRedirect();
+  } catch (err) {
+    console.error("Error al cerrar sesión:", err);
 
-  // Limpiar almacenamiento local y de sesión
-  localStorage.clear();
-  sessionStorage.clear();
-
-  // Redirigir a la URL de logout de Cognito
-  const logoutUrl = `${domain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutRedirect)}`;
-  window.location.href = logoutUrl;
+    // Fallback manual
+    localStorage.clear();
+    sessionStorage.clear();
+    const domain = 'https://us-east-1peioypcf6.auth.us-east-1.amazoncognito.com';
+    const clientId = '1a43cn452jearoj6siqsnj70g5';
+    const logoutRedirect = 'https://sistema-reservas-frontend.s3.us-east-1.amazonaws.com/index.html';
+    const logoutUrl = `${domain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutRedirect)}`;
+    window.location.href = logoutUrl;
+  }
 })();
