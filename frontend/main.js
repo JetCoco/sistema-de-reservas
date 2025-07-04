6885153c1a550bb205859814eef7be4a677f8e40
Eduardo Torrentera <eduardo.torrentera.colin@gmail.com>
2025-07-03 20:44:29 -0600

    fix token

--- frontend/main.js
+++ frontend/main.js
@@ -1,14 +1,19 @@
+// main.js
 import { UserManager } from "https://cdn.skypack.dev/oidc-client-ts";
 
 const cognitoAuthConfig = {
-  authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_peIOypcf6", // <-- Este sí funciona
+  authority: "https://us-east-1peioypcf6.auth.us-east-1.amazoncognito.com", // Dominio de Cognito
   client_id: "1a43cn452jearoj6siqsnj70g5",
   redirect_uri: "https://sistema-reservas-frontend.s3.us-east-1.amazonaws.com/login-success.html",
-  response_type: "code",
+  response_type: "code", // Authorization Code Grant
   scope: "openid email profile"
 };
 
 export const userManager = new UserManager(cognitoAuthConfig);
+
+export async function signInRedirect() {
+  await userManager.signinRedirect();
+}
 
 export async function signOutRedirect () {
   const logoutUri = "https://sistema-reservas-frontend.s3.us-east-1.amazonaws.com/index.html";
