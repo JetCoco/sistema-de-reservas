67bd39519ad1e0437cdc667fb6d40ae3d006d550
Eduardo Torrentera <eduardo.torrentera.colin@gmail.com>
2025-07-03 19:08:03 -0600

    fix:token validations

--- frontend/login.js
+++ frontend/login.js
@@ -1,5 +1,3 @@
-// login.js
-
 function decodeJwt(token) {
   const payload = token.split('.')[1];
   const decoded = atob(payload.replace(/-/g, '+').replace(/_/g, '/'));
@@ -11,21 +9,17 @@ function decodeJwt(token) {
   const params = new URLSearchParams(hash);
   const idToken = params.get('id_token');
   const accessToken = params.get('access_token');
-  const refreshToken = params.get('refresh_token');
 
-  if (!idToken) {
+  if (!idToken || !accessToken) {
     console.error('Token no encontrado en la URL.');
     window.location.href = "index.html";
     return;
   }
 
   try {
-    // Guardar tokens en almacenamiento local
     localStorage.setItem("idToken", idToken);
     localStorage.setItem("accessToken", accessToken);
-    localStorage.setItem("refreshToken", refreshToken);
 
-    // Decodificar el token para extraer el rol
     const decoded = decodeJwt(idToken);
     const email = decoded.email || decoded["cognito:username"];
     const role = decoded["custom:role"];
@@ -33,7 +27,6 @@ function decodeJwt(token) {
     localStorage.setItem("email", email);
     localStorage.setItem("role", role);
 
-    // Redirección según el rol
     if (role === "admin") {
       window.location.href = "admin.html";
     } else if (role === "instructor") {
