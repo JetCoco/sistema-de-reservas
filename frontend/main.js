98f58665a8806fa0dc104f931c7efb2df9f3ef13
Eduardo Torrentera <eduardo.torrentera.colin@gmail.com>
2025-07-03 20:27:20 -0600

    fix token

--- frontend/main.js
+++ frontend/main.js
@@ -1,8 +1,7 @@
-// main.js
 import { UserManager } from "https://cdn.skypack.dev/oidc-client-ts";
 
 const cognitoAuthConfig = {
-  authority: "https://us-east-1peioypcf6.auth.us-east-1.amazoncognito.com", // <-- ✅ ESTE es el correcto
+  authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_peIOypcf6",
   client_id: "1a43cn452jearoj6siqsnj70g5",
   redirect_uri: "https://sistema-reservas-frontend.s3.us-east-1.amazonaws.com/login-success.html",
   response_type: "code",
@@ -11,6 +10,8 @@ const cognitoAuthConfig = {
 
 export const userManager = new UserManager(cognitoAuthConfig);
 
-export async function signOutRedirect() {
-  await userManager.signoutRedirect();
+export async function signOutRedirect () {
+  const logoutUri = "https://sistema-reservas-frontend.s3.us-east-1.amazonaws.com/index.html";
+  const cognitoDomain = "https://us-east-1peioypcf6.auth.us-east-1.amazoncognito.com";
+  window.location.href = `${cognitoDomain}/logout?client_id=${cognitoAuthConfig.client_id}&logout_uri=${encodeURIComponent(logoutUri)}`;
 }
